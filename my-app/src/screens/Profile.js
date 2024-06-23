aaa58b2ee3b8127725a1d3c1a3bf10143fb99220
Ramiro Benacedo <ramirobenacedo@MacBook-Pro-de-Ramiro.local>
2024-06-23 15:30:11 -0300

    mi perfil funciona

--- my-app/src/screens/Profile.js
+++ my-app/src/screens/Profile.js
@@ -1,43 +1,145 @@
-import { Text, View, TouchableOpacity } from 'react-native'
+import { Text, View, TouchableOpacity, ScrollView, FlatList } from 'react-native'
 import React, { Component } from 'react'
-import { auth } from '../firebase/config'
+import Post from '../components/Post'
+import { db, auth } from '../firebase/config'
 
 export default class Profile extends Component {
     constructor(props){
         super(props)
         this.state = {
-            estaLogueado: false
+            estasLogueado: false,
+            posteos: [],
+            users: [],
+            eliminarPost: false
         }
     }
-    componentDidMount(){
-        auth.onAuthStateChanged((user) => {
-            if(user){
-                this.setState({estaLogueado: true})
-            }
-        })
+
+    componentDidMount() {
+        const currentUser = auth.currentUser;
+        if (currentUser) {
+          this.setState({ estasLogueado: true });
+          db.collection("posts")
+            .where("owner", "==", currentUser.email)
+            .onSnapshot((docs) => {
+              let posteosObtenidos = [];
+              docs.forEach(doc => {
+                posteosObtenidos.push({
+                  id: doc.id,
+                  data: doc.data()
+                });
+              });
+              this.setState({
+                posteos: posteosObtenidos
+              });
+            });
+          db.collection("users")
+            .where("owner", "==", currentUser.email)
+            .onSnapshot((docs) => {
+              let arrayUsers = [];
+              docs.forEach(doc => {
+                arrayUsers.push({
+                  id: doc.id,
+                  data: doc.data()
+                })
+              })
+              this.setState({
+                users: arrayUsers
+              })
+            })
+        }
     }
-    cerrarSesion(){
+
+    eliminarPost(postId) {
+        db.collection("posts")
+          .doc(postId)
+          .delete()
+    
+      }
+
+      eliminarUsuario() {
+        db.collection("users").where("owner", "==", auth.currentUser.email).get()
+          .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => doc.ref.delete() 
+              .then(() => {
+                auth.currentUser.delete() 
+                  .then(() => this.props.navigation.navigate('register'))
+                  .catch((error) => console.log("Error al eliminar al usuario del auth", error))
+              })
+              .catch((error) => console.log("Error al eliminar al usuario de la colección", error))
+            )
+          })
+          .catch((error) => console.log("Error al acceder a la colección", error))
+    
+      }
+
+    cerrarSesion() {
         auth.signOut()
-        .then(resp => console.log(resp))
-        .catch(err => console.log(err))
+          .then(() => {
+            this.setState({ estaLogueado: false });
+            this.props.navigation.navigate('login'); 
+          }).catch((error) => {
+            console.error('Error al cerrar sesión:', error);
+          });
+      }
+
+      render() {
+        const user = this.state.users.length > 0 ? this.state.users[0].data : null;
+        return (
+          <ScrollView >
+            {
+              user ?
+                <View >
+                  <Text >Mi Perfil</Text>
+                  <Text >{user.name}</Text>
+                  <Text >{user.owner}</Text>
+                  <Text >{user.minBio}</Text>
+                </View>
+                :
+                <Text>Cargando ...</Text>
+            }
+    
+            <View >
+              <Text >Tus Posteos:{this.state.posteos.length}</Text>
+              {
+                this.state.posteos.length > 0
+                  ?
+                  <View >
+                    <FlatList
+                      data={this.state.posteos}
+                      keyExtractor={(item) => item.id.toString()}
+                      renderItem={({ item }) =>
+                        <View >
+                          <Post post={item} />
+                          <TouchableOpacity onPress={() => this.eliminarPost(item.id)}>
+                            <Text > Eliminar posteo</Text>
+                          </TouchableOpacity>
+                        </View>
+                      }
+                    />
+                  </View>
+                  :
+                  <Text > El usuario no tiene posteos</Text>
+              }
+    
+            </View>
+    
+            <View >
+              <TouchableOpacity
+                onPress={() => this.cerrarSesion()}
+               
+              >
+                <Text >Cerrar Sesión</Text>
+              </TouchableOpacity>
+    
+              <TouchableOpacity
+                onPress={() => this.eliminarUsuario()}
+              >
+                <Text >Eliminar usuario</Text>
+              </TouchableOpacity>
+            </View>
+    
+          </ScrollView >
+        );
+      }
     }
-    render() {
-    return (
-        <View>
-        <Text>Perfil del usuario</Text>
-        {
-            this.state.estaLogueado ?
-            <TouchableOpacity
-                onPress={()=> this.cerrarSesion()}
-            >
-                <Text>Cerrar sesión</Text>
-            </TouchableOpacity>
-            :
-            <TouchableOpacity>
-                <Text>Iniciar sesion</Text>
-            </TouchableOpacity>
-        }
-      </View>
-    )
-  }
-}
+    
